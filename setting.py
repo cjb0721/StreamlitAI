865c9be95dfe12f17e0ba9cabfa3d451739cdd68
擎天小猪♥ <44831626+cjb0721@users.noreply.github.com>
2023-07-20 16:47:51 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -2,3 +2,5 @@ import os
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(BASE_PATH, "data")
+
+OPEN_API_KEY = "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"
