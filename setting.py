038bde64f9b13e587a52ddb5b9625bf353715ff0
擎天小猪♥ <44831626+cjb0721@users.noreply.github.com>
2023-07-26 15:49:15 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,6 +1,12 @@
 import os
+import base64
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(BASE_PATH, "data")
 
-OPEN_API_KEY = "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"
+os.environ["OPENAI_API_TYPE"] = "azure"
+os.environ["OPENAI_API_BASE"] = "https://bcrdcaitest.openai.azure.com/"
+os.environ["OPENAI_API_KEY"] = base64.b64decode(
+    "OTI4MTE4ZmNmMTYwNDZiMGExMjQ5Njk3NTY1OTRhZTA="
+).decode("utf-8")
+os.environ["OPENAI_API_VERSION"] = "2023-05-15"
