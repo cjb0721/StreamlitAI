e793ea498f470aab350b25e7e1548b29d3700e57
擎天小猪♥ <44831626+cjb0721@users.noreply.github.com>
2023-08-09 14:49:54 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -79,7 +79,7 @@ def get_file_content(file_name, path=DATA_DIR):
     return data
 
 
-def text_to_docs(text: str | List[str]) -> List[Document]:
+def text_to_docs():
     """
     Converts a string or list of strings to a list of Documents
     with metadata.
